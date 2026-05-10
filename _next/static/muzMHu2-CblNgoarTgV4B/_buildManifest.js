c7c336163e5edfe9939f7f75d747c426527480c9
EncryptedSam <sampathkumar.ajjapaga@gmail.com>
2026-05-11 03:06:29 +0530

    Updates

--- _next/static/muzMHu2-CblNgoarTgV4B/_buildManifest.js
+++ _next/static/muzMHu2-CblNgoarTgV4B/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sandg//_next/:path+",
+        "destination": "/sandg/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
